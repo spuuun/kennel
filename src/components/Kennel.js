b5e9d9c0e0f39294e53eb960e1b5d4ce766bb2a8
Matthew Ross <matthew.douglas.ross@gmail.com>
2019-08-23 13:34:10 -0500

    added files for new components

---
+++ src/components/Kennel.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import AnimalCard from './animal/AnimalCard'
+import './Kennel.css'
+
+class Kennel extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>Student Kennels<br />
+                        <small>Loving care when you're not there.</small>
+                    </h2>
+                    <address>
+                        Visit Us at the Nashville North Location
+            <br />500 Puppy Way
+          </address>
+                </div>
+                <div>
+                    <AnimalCard />
+                    <AnimalCard />
+                    <AnimalCard />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Kennel;
