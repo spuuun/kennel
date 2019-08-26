9b464c40a8e0fcaebdec430649979b73a147264d
Matthew Ross <matthew.douglas.ross@gmail.com>
2019-08-26 15:20:27 -0500

    basic files and functionality for ch 5 -- still need exercises for chs 3-5

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -20,13 +20,29 @@ class AnimalList extends Component {
             })
     }
 
+    deleteAnimal = id => {
+        AnimalManager.delete(id)
+            .then(() => {
+                AnimalManager.getAll()
+                    .then((newAnimals) => {
+                        this.setState({
+                            animals: newAnimals
+                        })
+                    })
+            })
+    }
+
     render() {
         console.log("AnimalList: Render");
 
         return (
             <div className="container-cards">
                 {this.state.animals.map(animal =>
-                    <AnimalCard key={animal.id} animal={animal} />
+                    <AnimalCard
+                        key={animal.id}
+                        animal={animal}
+                        deleteAnimal={this.deleteAnimal}
+                    />
                 )}
             </div>
         )
